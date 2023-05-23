1495bfb3d60b568939fe7a89e39e9274d1ac1144
timing1018 <73340657+timing1018@users.noreply.github.com>
2023-05-23 14:49:24 +0900

    first commit_file

---
+++ pages/create.tsx
@@ -0,0 +1,14 @@
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WEB!</title>
+      </Head>
+        <h2>Create</h2>
+        Hello, Create!
+    </>
+  )
+}
